246f9d6e082730fb20e708e367a1b0d714c18535
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-10-03 16:08:20 -0700

    added more stuff

---
+++ src/components/hooks/use-search-results.js
@@ -0,0 +1,53 @@
+import { object } from "prop-types";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { search } from "../services/services";
+import { useInView } from 'react-intersection-observer'
+
+
+export default function useSearchResults() {
+    const [searchResults, setSearchResults] = useState([]);
+    const [error, setError] = useState('');
+    const [searchParams, setSearchParams] = useSearchParams();
+    const usableSearchParams = object.fromEntries(searchParams.entries());
+
+    const nextPage = async () => {
+        usableSearchParams.page = parseInt(usableSearchParams.page) + 1;
+        setSearchParams(usableSearchParams);
+        const moreResults = await search(usableSearchParams);
+        setSearchResults(searchResults.concat(moreResults.results))
+    }
+
+    const infiniteScrollRef = useInView({
+        triggerOnce: true,
+        onChange: (inView) => {
+            if (inView) nextPage();
+        },
+    }).ref;
+
+    const searchPokedex = async (searchObj) => {
+        if (searchObj.page === null) {
+            searchObj.page = 1
+        }
+        setSearchParams(searchObj);
+
+        try {
+            const body = await search(searchObj);
+            setSearchResults(body.results);
+        } catch (e) {
+            setError('Error' + e.body.toString());
+        }
+    }
+
+    useEffect(() => void searchPokedex(usableSearchParams), [])
+
+    return {
+        nextPage,
+        searchParams,
+        searchResults,
+        setSearchResults,
+        searchPokedex,
+        infiniteScrollRef
+    }
+
+}
